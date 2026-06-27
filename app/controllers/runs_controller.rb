4d835e5f336dec4097e7d0a43b811fe5f296599e
Barış Özmen <hbaristr@gmail.com>
2026-06-27 14:42:32 -0700

    Fix performance-2 passport tree query fanout
    Report: /Users/barisozmen/development/github/barisozmen/agent_control_room/docs/audits/performance/2026-06-27.md

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -14,7 +14,8 @@ class RunsController < ApplicationController
   def show
     @run = Run.find(params[:id])
     @runs = Run.session_list
-    @selected_passport = @run.selected_passport(params[:passport_id])
+    @passport_tree = @run.passport_tree
+    @selected_passport = @passport_tree.selected_passport(params[:passport_id])
     @panel = %w[passport audit].include?(params[:panel]) ? params[:panel] : nil
   end
 
